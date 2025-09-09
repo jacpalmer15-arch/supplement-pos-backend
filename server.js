af6b3ee940c5efcce5717ad4fa3a6e97eeec4d40
jacpalmer15-arch <jacpalmer15@gmail.com>
2025-09-09 18:21:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const cloverService = require('./services/cloverServices');
+const cloverService = require('./services/cloverService');
 const productRoutes = require('./routes/products');
 const inventoryRoutes = require('./routes/inventory');
 const checkoutRoutes = require('./routes/checkout');
