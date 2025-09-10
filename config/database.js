d5ee966307d297a9052588fbbc71bc2d276a34fa
jacpalmer15-arch <jacpalmer15@gmail.com>
2025-09-10 12:57:42 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,14 +8,5 @@ const pool = new Pool({
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
-// (Optional) quick connection test on cold start
-pool.connect()
-  .then(client => {
-    console.log('Database connected successfully');
-    client.release();
-  })
-  .catch(err => {
-    console.error('Database connection error:', err);
-  });
-
 module.exports = pool;
+
