d85365efe95bb0691d4ad08ba7c47eb66b0efe5c
jacpalmer15-arch <jacpalmer15@gmail.com>
2025-09-10 13:37:44 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
 // /api/index.js
 const app = require('../server');
-
-module.exports = (req, res) => {
-  // index.js handles only /api (exact root). Make sure Express sees /api.
-  if (!req.url.startsWith('/api')) {
-    // Vercel calls this with req.url === '/' for /api root.
-    req.url = '/api' + (req.url === '/' ? '' : req.url);
-  }
-  return app(req, res);
-};
+module.exports = (req, res) => app(req, res);
