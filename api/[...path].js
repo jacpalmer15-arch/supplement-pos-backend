3e773ca15cd656a2a50a71aa92031c35bf274f6e
jacpalmer15-arch <jacpalmer15@gmail.com>
2025-09-10 13:37:31 -0400

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,8 +1,3 @@
 // /api/[...path].js
 const app = require('../server');
-
-module.exports = (req, res) => {
-  // For any /api/* subpath, re-prefix /api so your Express routes match.
-  req.url = '/api' + req.url;
-  return app(req, res);
-};
+module.exports = (req, res) => app(req, res);
